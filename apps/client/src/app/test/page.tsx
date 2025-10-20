8f7845e81e3737652dd0b71787b8b98a30f71a23
Zain Javed <zainjhere@gmail.com>
2025-10-20 15:22:21 +0500

    Clerk middleware with express

---
+++ apps/client/src/app/test/page.tsx
@@ -0,0 +1,18 @@
+import {auth} from "@clerk/nextjs/server";
+
+const TestPage = async () => {
+  const {getToken} = await auth();
+  const token = await getToken();
+
+  const res = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  const data = await res.json();
+
+  console.log(data);
+  return <div className="">TestPage</div>
+}
+
+export default TestPage;
