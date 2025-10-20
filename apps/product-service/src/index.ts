aa5ab548500cafa90c3aa0177b73ccf9ad42ef0b
Zain Javed <zainjhere@gmail.com>
2025-10-20 13:24:58 +0500

    get rid of turbopack as it was causing problem with monorepos

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors({
+  origin: ["http://localhost:3002", "http://localhost:3003"],
+  credentials: true
+}));
+
+app.listen(8000, () => {
+  console.log(`Product service is running...`);
+})
